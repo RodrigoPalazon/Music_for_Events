7cf859eed5e7f52a0d0f94eaca338cac9f624b4c
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-01 17:46:46 +0200

    Add navbar, uses <Link> component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/home'
 import { About } from './pages/about'
 import { Contact } from './pages/contact'
@@ -12,12 +12,28 @@ function App() {
 //   const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route path= '/about' element= {<About/>} />
-      <Route path= '/contact' element= {<Contact/>} />
-      <Route path= '/' element= {<Home/>} />
-      <Route path= '/price' element= {<Price/>} />
-    </Routes>
+    <>
+        <nav>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li>
+                <Link to='/price'>Price</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Contact</Link>
+            </li>
+        </nav>
+        <Routes>
+            <Route path= '/about' element= {<About/>} />
+            <Route path= '/contact' element= {<Contact/>} />
+            <Route path= '/' element= {<Home/>} />
+            <Route path= '/price' element= {<Price/>} />
+        </Routes>
+    </>
   )
 }
 
